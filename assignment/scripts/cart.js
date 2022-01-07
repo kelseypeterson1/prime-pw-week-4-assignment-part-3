940d52ffcd0e0b148328f080b91229fbc0dae564
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-01-07 13:49:32 -0600

    Created the listItems function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,27 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
 let basket = [];
+
+// Adding items to basket
 function addItem(item) {
   basket.push(item);
+  return true;
 }
+
+// Testing the addItem function
+console.log("Test - adding a pineapple using the addItem function", addItem("pineapple"));
+console.log("Basket now contains:", basket[0]);
+
+// Listing items in basket
+let index = 0;
+function listItems() {
+  while (index<basket.length) {
+    console.log(basket[index]);
+    index++;
+  }
+}
+
+console.log("Test - Listing items in basket:");
+listItems();
